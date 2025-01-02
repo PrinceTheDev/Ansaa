d4e8685d8da8437db16e5f612f67acb09675d6ff
PrinceTheDev <sinbadprince9@gmail.com>
2025-01-02 17:21:53 +0100

    Implementing the edit, continue and submit buttons

--- ansaa/src/app/components/permit/permit.component.ts
+++ ansaa/src/app/components/permit/permit.component.ts
@@ -1,23 +1,36 @@
 import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, } from '@angular/forms';
 import { PermitModel } from '../../model/permit';
 
 @Component({
   selector: 'app-permit',
   standalone: true,
-  imports: [DatePipe,ReactiveFormsModule],
+  imports: [DatePipe,ReactiveFormsModule, CommonModule],
   templateUrl: './permit.component.html',
   styleUrl: './permit.component.css'
 })
-export class PermitComponent {
+export class PermitComponent implements OnInit {
 
+  currentDate: Date = new Date();
   permitForm: FormGroup = new FormGroup({});
 
   permitObj: PermitModel = new PermitModel();
+  permitList: PermitModel[] = [];
+
+  showContinueButton: boolean  = true;
+  showEditButton: boolean = false;
+  showSubmitButton: boolean = false;
+  isEditable: boolean = false;
+
+  ngOnInit(): void {
+    
+  }
 
   constructor() {
     this.createForm();
+    
   }
 
   createForm() {
@@ -40,5 +53,43 @@ export class PermitComponent {
 
     })
   }
+
+  onContinue() {
+    // this will store the data temporarily in localStorage
+    localStorage.setItem('permitData', JSON.stringify(this.permitForm.value));
+
+    // display the stored data in the modal
+    const storedData = localStorage.getItem('permitData');
+    if (storedData) {
+    this.permitForm.patchValue(JSON.parse(storedData));
+    }
+
+    //update the modal controls
+    this.showContinueButton = false;
+    this.showEditButton = true;
+    this.showSubmitButton = true;
+    // This will make the data read only
+    this.isEditable = false;
+  }
+
+
+  onEdit(){
+    // Enable editing of the formm when clicked
+    this.isEditable = true;
+  }
+
+  onSubmit(){
+    // This will save the data permanentl on the localStorage
+    const editedData = this.permitForm.value;
+    localStorage.setItem('finalPermitData', JSON.stringify(editedData));
+    //redirect user to updated table with the form
+    // this.redirectToUpdatedTable
+  }
+
+  // redirectToUpdatedTable() {
+  //   this.router.navigate([/table]);
+  // }
+
+
 
 }
